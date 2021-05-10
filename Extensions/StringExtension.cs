6775e9cd29da12e2f05f4293eefb26163e50d1f7
andreas.imboden <andreas.imboden@europa3000.ch>
2021-05-10 17:04:08 +0200

    StringExtension

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace freaxnx01.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+    }
+}
