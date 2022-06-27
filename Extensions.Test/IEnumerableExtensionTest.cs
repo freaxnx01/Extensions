e10534afba1a4778326c42bef778557358bc2f24
e3k-dev <98828805+e3k-dev@users.noreply.github.com>
2022-06-27 09:47:28 +0200

    IEnumerableExtension

---
+++ Extensions.Test/IEnumerableExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using freaxnx01.Extensions;
+using Xunit;
+
+namespace Extensions.Test
+{
+    // ReSharper disable once InconsistentNaming
+    public class IEnumerableExtensionTest
+    {
+        [Fact]
+        public void SafeListHasElementTest()
+        {
+            var theList = new List<string>() { "text" };
+            Assert.True(theList.Safe().Any());
+        }
+
+        [Fact]
+        public void SafeListNoElementTest()
+        {
+            var theList = new List<string>();
+            Assert.False(new List<string>().Safe().Any());
+        }
+
+        [Fact]
+        public void SafeListNullTest()
+        {
+            List<string> theList = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.False(theList.Safe().Any());
+        }
+    }
+}
---
+++ Extensions/IEnumerableExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace freaxnx01.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        // Source: https://stackoverflow.com/questions/24390005/checking-for-empty-or-null-liststring
+        public static IEnumerable<T> Safe<T>(this IEnumerable<T> source)
+        {
+            
+            if (source == null)
+            {
+                yield break;
+            }
+
+            foreach (var item in source)
+            {
+                yield return item;
+            }
+        }
+    }
+}
